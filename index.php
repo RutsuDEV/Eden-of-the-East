0a9b4e1bc1ac6301990c33fb3ca372f8e9a0afdd
Heniooo <admin@henicz.pl>
2017-06-08 21:35:36 +0200

    Little code changes

--- index.php
+++ index.php
@@ -4,31 +4,75 @@
 <meta charset="utf-8">
 <title>Eden of the East</title>
 <link href="system/css/basic.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 </head>
 <body>
+<section class="edenSplash">
+			<div class="edenLogo">
+				<img src="system/images/eden.png">
+			</div>
+</section>
 <audio id="eden" src="system/eden.mp3" type="audio/mp3">Your browser does not support the &#60;audio&#62; element.</audio>
+<div class="musicPlayer">
+	<div class="button  startMusic">Start music</div>
+</div>
 	<div class="internetError" title="Ponów próbe">Brak połączenia z Internetem</div>
 	<div class="internetLoading" title="Trwa łączenie...">Łączenie...</div>
 	<div class="internetConnected" title="Połączono">Połączono</div>
 	<section class="eden">
-		<div class="edenLogo">
-			<img src="system/images/eden.png">
-		</div>
+		<center>
+			<div class="edenLogo">
+				<img src="system/images/eden.png">
+			</div>
+		</center>
 		<div class="edenSearch">
-				<input type="text" name="q" placeholder="Search in eden" autocomplete="off">
+				<input type="text" name="q" autocomplete="off">
+				<div class="searchButtons">
+					<center>
+						<button class="searchButton">Search  in eden</button>
+						<button class="edenAboutButton">About eden</button>
+					</center>
+				</div>
 		</div>
 	</section>
+
 	<section class="edenResults">
+
 	</section>
+
 	<section class="resultView">
 	
 	</section>
-<a class="startMusic" href="#start">Start music</a>
-<a class="pauseMusic" href="#stop">Stop music</a>
+	
+	<section class="loadingResults">
+		  <div class="sk-double-bounce">
+			<div class="sk-child sk-double-bounce1"></div>
+			<div class="sk-child sk-double-bounce2"></div>
+		  </div>
+		<div class="loaderText">Loading results</div>
+	</section>
+	
+	<section class="edenAbout">
+		<div class="aboutBlock">
+			<div class="edenLogo">
+				<img src="system/images/eden.png">
+			</div>
+			<div class="aboutText"><center>Specialized search engine looking for different data in eden. The search engine is based on the anime "Higashi no Eden"</center></div>
+			<div class="aboutWallpaper"></div>
+		</div>
+	</section>
 <script src="system/js/jquery.js"></script>
 
 <script>
+$(document).ready(function(){
+	$(".edenSplash").fadeOut("slow");
+})
+$(".edenAboutButton").on('click', function(){
+	$("section.edenAbout").fadeIn("slow");
+});
+$("section.edenAbout").on('click', function(){
+	$("section.edenAbout").fadeOut("slow");
+});
 function checkInternet(){
 	var checkInternet = setInterval(function(){
 				$.ajax({
@@ -56,16 +100,80 @@ $(document).ready(function(){
 	checkInternet();
 	console.log("CHECK INTERNET");
 });
+   $('.edenSearch .searchButton').on('click', function (e) {
+	   $(".loadingResults").css("display", "block");
+	console.log("button  PRESS");
+
+            $(".edenSearch input").attr("disabled", "disabled");
+            $(".edenSearch input").removeAttr("disabled");
+
+
+			var value = $(".edenSearch input").val();
+			var data = {q:value};
+$.ajax({
+    type     : "POST",
+    url      : "system/ajax/search.php",
+	cache: null,
+    data     : data,
+    success: function(res) {
+		$(".loadingResults").fadeOut("slow");
+		$(".internetError").fadeOut("fast");
+		$(".edenResults").html(res);
+		$(".resultItem").on('click', function(){
+			var id = $(this).attr("data-id");
+			var data2 = {id:id};
+				$.ajax({
+					type     : "POST",
+					url      : "system/ajax/data/get.data.php",
+					cache: null,
+					data     : data2,
+					success: function(res) {
+						$(".internetError").fadeOut("fast");
+						$(".resultView").html(res);
+						$(".resultView").fadeIn("fast");
+						$(".resultView").on('click', function(){
+							$(".resultView").fadeOut("fast");
+							$(".resultView").html("");
+						});
+						
+						$(".resultBlock").on('click', function(){
+							return false;
+						});
+						
+						$(".blockClose").on('click', function(){
+							$(".resultView").fadeOut("fast");
+							$(".resultView").html("");
+						});
+					},
+					complete: function() {
+
+					},
+					error: function(jqXHR, errorText, errorThrown) {
+						$(".loadingResults").fadeOut("slow");
+						$(".internetError").fadeIn("fast");
+					}
+				});
+		});
+    },
+    complete: function() {
+
+    },
+    error: function(jqXHR, errorText, errorThrown) {
+		$(".internetError").fadeIn("fast");
+    }
+});
+			
+			$(".edenResults").fadeIn("slow");
+
+   });
+   
    $('.edenSearch input').on('keypress', function (e) {
 	console.log("input  PRESS");
          if(e.which === 13){
+			$(".loadingResults").css("display", "block");
 			console.log("enter PRESSED");
 
             $(this).attr("disabled", "disabled");
-
-            //Do Stuff, submit, etc..
-
-            //Enable the textbox again if needed.
             $(this).removeAttr("disabled");
 			var value = $(".edenSearch input").val();
 			var data = {q:value};
@@ -75,6 +183,7 @@ $.ajax({
 	cache: null,
     data     : data,
     success: function(res) {
+		$(".loadingResults").fadeOut("slow");
 		$(".internetError").fadeOut("fast");
 		$(".edenResults").html(res);
 		$(".resultItem").on('click', function(){
@@ -96,6 +205,11 @@ $.ajax({
 						$(".resultBlock").on('click', function(){
 							return false;
 						});
+						
+						$(".blockClose").on('click', function(){
+							$(".resultView").fadeOut("fast");
+							$(".resultView").html("");
+						});
 					},
 					complete: function() {
 
@@ -110,6 +224,7 @@ $.ajax({
 
     },
     error: function(jqXHR, errorText, errorThrown) {
+		$(".loadingResults").fadeOut("slow");
 		$(".internetError").fadeIn("fast");
     }
 });
@@ -121,15 +236,25 @@ $.ajax({
 
 
 
-$(".startMusic").on('click', function(){
-var vid = document.getElementById("eden");
-vid.play();
-})
 
-$(".pauseMusic").on('click', function(){
-var vid = document.getElementById("eden");
-vid.pause();
-});
+
+$(".startMusic, .pauseMusic").click(function()
+    {
+        if ($(this).hasClass('startMusic')) {
+			var vid = document.getElementById("eden");
+			vid.play();
+			$(this).html("Stop music");
+            $(this).removeClass('startMusic').addClass('pauseMusic');
+        } else {
+			var vid = document.getElementById("eden");
+			vid.pause();
+            $(this).html("Start music");
+            $(this).removeClass('pauseMusic').addClass('startMusic');
+        }
+    }   
+);
+
+
 
 $(".internetError").on('click', function(){
 	console.log("LOADING #1");
--- system/class/data.class.php
+++ system/class/data.class.php
@@ -17,7 +17,16 @@
 			while($row = mysqli_fetch_object($result)){
 echo '
 <article class="resultBlock">
+	<div class="blockClose"></div>
 	<div class="resultItem" data-id="'.$row->ID.'">
+	<div class="itemImages">';
+    		$sql_images = "SELECT * FROM images WHERE data_id = '$id' ORDER BY ID LIMIT 10";
+	        $result_images = mysqli_query($this->db,$sql_images);
+while($image = mysqli_fetch_object($result_images)){
+echo '<img src="/eden-of-the-east'.$image->src.'">';
+}
+echo '
+	</div>
 		<div class="itemLink"><a href="#">'; echo htmlspecialchars($row->name, ENT_QUOTES, 'UTF-8'); echo '</a></div>
 		<div class="itemDescription">'; echo htmlspecialchars($row->description, ENT_QUOTES, 'UTF-8'); echo '</div>
 	</div>
